33840fb40d1c01c9dbd7900514738dbb7328a14e
klam93 <82006016+klam93@users.noreply.github.com>
2024-07-25 23:56:58 -0500

    Commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const margin = { top: 50, right: 50, bottom: 50, left: 50 },
       width = 1500 - margin.left - margin.right,
-      height = 700 - margin.top - margin.bottom;
+      height = 600 - margin.top - margin.bottom;
 
 const svg = d3.select("svg")
               .attr("width", width + margin.left + margin.right)
